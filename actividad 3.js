fead8d77d42946681bdc76c220f496abe4e0679d
candicelopezj <96924625+candicelopezj@users.noreply.github.com>
2022-03-17 14:50:33 -0700

    Update actividad 3.js

--- actividad 3.js
+++ actividad 3.js
@@ -1,3 +1,4 @@
+//Ejercicio 1//
 var num = 0;
 num = prompt("ingrese un numero");
 if(num %2===0) {
@@ -7,3 +8,45 @@ if(num %2===0) {
     alert("su numero no es divisible entre 2");
     document.write("su no es numero es divisible entre 2 : porque usaste:" + num + "<br>");
 }   
+
+//Ejercicio 2//
+var num = 0;
+num = prompt ("introduce un num");
+
+
+if (num %2===0) {
+    alert("si es un numero par");
+}else{
+     
+     alert("no es un numero par"); 
+    
+
+//Ejercicio 3//
+ var num = 0;
+num = prompt ("introduce un num");
+if (num = 1000) {
+    document.write("ganaste un premio :" + "<br>");
+}else {
+   alert("Lo sentimos, sigue participando");
+}
+    
+//Ejercicio 4//
+var num1 = prompt("introduce el primer numero");
+var num2 = prompt("introduce el segundo numero");
+if(num1 < num2){
+    alert(num1 + "es menor que" + num2 )
+}else{
+    alert(num2 + "es menor que" + num1 )
+} 
+    
+//Ejercicio 5//    
+var num1 = prompt("introduce el primer numero");
+var num2 = prompt("introduce el segundo numero");
+var num3 = prompt("introduce el tercer numero");
+if(num1 > num2 && num1 > num3){
+    alert("el numero mayor es " + num1);
+}else if(num2 > num1 && num2 > num3){
+    alert("el numero mayor es " + num2);
+}else{
+     alert("el numero mayor es " + num3);
+}
